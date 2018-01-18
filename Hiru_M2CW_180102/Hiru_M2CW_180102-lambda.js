f9a2b6ddfdc65cf7a1d80d64d7bb91a7fe2a5206
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-18 12:06:22 +0530

    second commit

--- Hiru_M2CW_180102/Hiru_M2CW_180102-lambda.js
+++ Hiru_M2CW_180102/Hiru_M2CW_180102-lambda.js
@@ -4,17 +4,17 @@ exports.handler = function (event, context, callback) {
 
 
 	kinesis.putRecord({
-		Data: 'test message',
+		Data: 'test message sample',
 		PartitionKey: '001',
 		StreamName: 'Hiru_test'
 	}).promise()
 		.then(putRecordData => {
-			console.log('record data',putRecordData);
-			callback(null,putRecordData );
+			console.log('record data', putRecordData);
+			callback(null, putRecordData);
 		})
 		.catch(err => {
-			console.log('error',err);
-			callback(null,err );
+			console.log('error', err);
+			callback(null, err);
 		});
 
 	//callback(null, 'Successfully executed');
