a46d14920b61fc0a1f2b3cfaf7d9210ad1d13b63
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-18 11:41:49 +0530

    initial commit

---
+++ Hiru_M2CW_180102/Hiru_M2CW_180102-lambda.js
@@ -0,0 +1,21 @@
+let AWS = require('aws-sdk');
+const kinesis = new AWS.Kinesis();
+exports.handler = function (event, context, callback) {
+
+
+	kinesis.putRecord({
+		Data: 'test message',
+		PartitionKey: '001',
+		StreamName: 'Hiru_test'
+	}).promise()
+		.then(putRecordData => {
+			console.log('record data',putRecordData);
+			callback(null,putRecordData );
+		})
+		.catch(err => {
+			console.log('error',err);
+			callback(null,err );
+		});
+
+	//callback(null, 'Successfully executed');
+}
